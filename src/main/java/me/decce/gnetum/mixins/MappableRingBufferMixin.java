c275b2801ef11476011ce05b6a4b6bff6038cebb
decce6 <decce6@proton.me>
2026-08-09 07:05:00 +0800

    Make MappableRingBufferMixin optional

--- src/main/java/me/decce/gnetum/mixins/MappableRingBufferMixin.java
+++ src/main/java/me/decce/gnetum/mixins/MappableRingBufferMixin.java
@@ -9,15 +9,15 @@ import org.spongepowered.asm.mixin.injection.At;
 //? >=1.21.10 {
 // We require additional buffer flushing before switching framebuffers. To avoid client side stall when awaiting
 // fence completion on glClientWaitSync, we increase the buffer count here.
-@Mixin(net.minecraft.client.renderer.MappableRingBuffer.class)
+@Mixin(value = net.minecraft.client.renderer.MappableRingBuffer.class, priority = 2000)
 public class MappableRingBufferMixin {
-    @ModifyExpressionValue(method = { "<init>", "close" } , at = @At(value = "CONSTANT", args = "intValue=3"))
+    @ModifyExpressionValue(method = { "<init>", "close" } , at = @At(value = "CONSTANT", args = "intValue=3"), require = 0, expect = 0)
     private int gnetum$increaseBufferSize(int original) {
         // We must generate a size that is the largest we might need because we cannot resize afterward
         return original + 6;
     }
 
-    @ModifyExpressionValue(method = "rotate" , at = @At(value = "CONSTANT", args = "intValue=3"))
+    @ModifyExpressionValue(method = "rotate" , at = @At(value = "CONSTANT", args = "intValue=3"), require = 0, expect = 0)
     private int gnetum$rotate$increaseBufferSize(int original) {
         if (!Gnetum.config.isEnabled()) {
             return original;
